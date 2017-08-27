fcd5d5e10e7cf8fe42c8030521c63748863c07a4
David <dbstern@gwmail.gwu.edu>
2017-08-27 10:34:14 -0400

    pgls script ok

--- run_pgls.R
+++ run_pgls.R
@@ -2,18 +2,21 @@ library(ape)
 library(geiger)
 library(nlme)
 library(phytools)
+library(stats)
 
 outfile <- "PhyloDE_results.csv"
 write.csv('Gene_Family,Coef,P-value', file=outfile, row.names=FALSE, col.names=FALSE, append=TRUE)
 
 pgls_loop <- function(treefile){
 	gene <- strsplit(treefile,'\\.')[[1]][1]
-	dat <- read.csv(paste(gene, ".dat.csv", sep=""), header=T)
+	dat <- read.csv(paste(gene, ".dat.csv", sep=""), header=T, row.names=1)
 	phy <- read.tree(treefile)
+	phy$edge.length <- phy$edge.length * 100
 	phy <- midpoint.root(phy)
-	mod <- gls(State ~ Expression, correlation = corMartins(1, phy = phy), data = dat)
+	w<-diag(vcv.phylo(phy))
+	mod <- gls(State ~ Expression, correlation = corMartins(1, phy = phy, fixed=FALSE), data = dat, weights=varFixed(~w))
 	ans <- anova(mod)
-	df <- data.frame(gene, mod$coefficients[2], ans$p-value[2])
+	df <- data.frame(gene, mod$coefficients[2], ans$'p-value'[2])
 	write.table(df, file=outfile, row.names=FALSE, col.names=FALSE, sep=",", append=TRUE)
 	}
 
